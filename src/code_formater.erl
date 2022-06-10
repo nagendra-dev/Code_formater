c42a9f2fa5c2388346f72edc99a22c289180b46e
nagendra_dev <nagendradev1495@gmail.com>
2022-06-11 00:25:48 +0530

    Added example formater file

---
+++ src/code_formater.erl
@@ -0,0 +1,2 @@
+-module(code_formater). -export([arithmatic_operation/3]). arithmatic_operation(Operation, Init, Elements) when Elements /= [], Operation == add; Operation == mul ->
+Fun = fun(Value, Output) -> case Operation of add -> Value + Output; mul -> Value * Output end end, lists:foldl(Fun, Init, Elements).
